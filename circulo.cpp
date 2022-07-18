2ccfdf0292883ba6b3c391ee0ea6533f942fa95e
Geovanny Jaramillo <byronjaramillo_@hotmail.com>
2022-07-18 10:01:58 -0500

    Agregado circulo

--- circulo.cpp
+++ circulo.cpp
@@ -3,11 +3,30 @@
 Circulo::Circulo()
     :QPoint(0,0)
 {
-
+ lienzo = QPixmap(500,500);
+  this->dibujar();
 }
 
 Circulo::Circulo(int xpos, int ypos)
     :QPoint(xpos, ypos)
 {
 
+    xpos=100;
+    ypos=100;
+}
+
+void Circulo::paint(QPainter &painter)
+{
+
+}
+
+void Circulo::dibujar()
+{
+    lienzo.fill(Qt::white);
+
+        QPainter painter(&lienzo);
+
+
+
+         painter.drawEllipse(100,100,100,100);
 }
--- circulo.h
+++ circulo.h
@@ -2,12 +2,17 @@
 #define CIRCULO_H
 
 #include <QPoint>
+#include<QPainter>
+#include<QPixmap>
 
 class Circulo : public QPoint
 {
 public:
     Circulo();
     Circulo(int xpos, int ypos);
+    void paint(QPainter &painter);
+     QPixmap lienzo;
+      void dibujar();
 };
 
 #endif // CIRCULO_H
